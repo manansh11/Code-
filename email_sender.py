7bba3c1d9fcf5733eabbf6eaafbba8aec89f72ff
Manansh Shukla <47335622+manansh11@users.noreply.github.com>
2020-05-13 21:34:10 -0600

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -4,16 +4,16 @@ from email.message import EmailMessage
 
 #Create EmailMessage Object (Instantiate)
 email = EmailMessage()
-email['from'] = 'Manansh Shukla'
-email['to'] = 'divy32@outlook.com'
+email['from'] = 'Your Name'
+email['to'] = 'hello@gmail.com'
 email['subject'] = 'Hello' 
 
 #Content of email
-email.set_content('You are gay')
+email.set_content('Write a message here')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls() #Encryption Mechanism
-    smtp.login('calgarycartcorp@gmail.com', 'heythecarthasfourrings11!')
+    smtp.login('gmail@gmail.com', 'password')
     smtp.send_message(email)
     print("Email Sent")
